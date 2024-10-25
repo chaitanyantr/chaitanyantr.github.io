23ca9037fc8f63ce5a3b24616a47927ba13e74b9
Abhay D <a.deshpande012@gmail.com>
2024-10-24 18:13:36 -0700

    Add tag size field

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
 const mappings = {
     "tag16h5": {
         "file_prefix": "tag16_05_",
-        "viewBox": "0 0 80 80"
+        "viewBox": "0 0 80 80",
+		"tag_size_ratio": 6/8
     },
     "tag25h9": {
         "file_prefix": "tag25_09_",
-        "viewBox": "0 0 90 90"
+        "viewBox": "0 0 90 90",
+		"tag_size_ratio": 7/9
     },
     "tag36h11": {
         "file_prefix": "tag36_11_",
-        "viewBox": "0 0 100 100"
+        "viewBox": "0 0 100 100",
+		"tag_size_ratio": 8/10,
     },
     "tagCircle21h7": {
         "file_prefix": "tag21_07_",
-        "viewBox": "0 0 90 90"
+        "viewBox": "0 0 90 90",
+		"tag_size_ratio": 1.0
     },
     "tagCircle49h12": {
         "file_prefix": "tag49_12_",
-        "viewBox": "0 0 110 110"
+        "viewBox": "0 0 110 110",
+		"tag_size_ratio": 1.0
     },
     "tagCustom48h12": {
         "file_prefix": "tag48_12_",
-        "viewBox": "0 0 100 100"
+        "viewBox": "0 0 100 100",
+		"tag_size_ratio": 6/10,
     },
     "tagStandard41h12": {
         "file_prefix": "tag41_12_",
-        "viewBox": "0 0 90 90"
+        "viewBox": "0 0 90 90",
+		"tag_size_ratio": 5/9,
     },
     "tagStandard52h13": {
         "file_prefix": "tag52_13_",
-        "viewBox": "0 0 100 100"
+        "viewBox": "0 0 100 100",
+		"tag_size_ratio": 6/10
     }
 }
 
@@ -65,14 +73,33 @@ function init() {
 	var familySelect = document.querySelector('.setup select[name=dict]');
 	var markerIdInput = document.querySelector('.setup input[name=id]');
 	var sizeInput = document.querySelector('.setup input[name=size]');
+	var tagSizeInput = document.querySelector('.setup input[name=tag-size]');
 	var saveButton = document.querySelector('.save-button');
-	
+
 	function updateSize(){
 		var size = Number(sizeInput.value);
-		const svgElm = document.querySelector('.marker').firstElementChild
-		svgElm.setAttribute('width', size + 'mm');
-		svgElm.setAttribute('height', size + 'mm');
+		const svgElm = document.querySelector('.marker').firstElementChild;
+		if (!!svgElm) {
+			svgElm.setAttribute('width', size + 'mm');
+			svgElm.setAttribute('height', size + 'mm');
+		}
 	}
+
+	familySelect.addEventListener("change", function() {
+		sizeInput.dispatchEvent(new Event('change'));
+	});
+
+	sizeInput.addEventListener("change", function() {
+		var familyName = familySelect.options[familySelect.selectedIndex].value;
+		tagSizeInput.value = Math.round(this.value * mappings[familyName].tag_size_ratio * 10) / 10;
+		updateSize();
+	});
+	
+	tagSizeInput.addEventListener("change", function() {
+		var familyName = familySelect.options[familySelect.selectedIndex].value;
+		sizeInput.value = Math.round(this.value / mappings[familyName].tag_size_ratio * 10) / 10;
+		updateSize();
+	});
 
 	var updateTag = debounce(function() {
 		var sizeid = Number(sizeInput.value);
@@ -89,6 +116,7 @@ function init() {
 		})
 	}, 200)
 
+	sizeInput.dispatchEvent(new Event('change'));
 	updateTag();
 
 	familySelect.addEventListener('change', updateTag);
